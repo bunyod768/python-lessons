4620d836bf0e960fcc65fa189fbc703bea89a85a
Bunyod <b8515574@gmail.com>
2025-02-13 10:47:53 +0500

    while (new)
    changed

---
+++ while.py
@@ -0,0 +1,65 @@
+#parol = qwerty1234
+# parol = input("parolni kiriting:")
+# while(True):
+#     if(parol == "qwerty1234"):
+#         print("tizimga kirish mumkin:)")
+#         break
+#     parol = input("parol xato qayta kiriting:(")
+
+#yarim uchburchak yasash
+# height = int(input("Uchburchak balandligi >>"))
+# i=0
+# while(i <= height):
+#     print("*" * i)
+#     i+=1
+
+# 1-mashq
+# i=1
+# while True:    
+#     book = input(f"Yoqtirgan kitobim:({i}):")
+#     savol=input("yana qo'shasizmi?(ha/yo'q)")
+#     i+=1
+#     if savol != "ha":
+#         break
+# print("dastur tugadi.")
+# 2-mashq
+
+# while True:
+#     yosh = int(input("Yoshingizni kiriting:"))
+#     if(yosh < 7):
+#         chipta = 2000
+#     elif yosh < 18:
+#         chipta = 3000
+#     elif yosh <65:
+#         chipta = 30000
+#     else:
+#         chipta = str(chipta)
+#         chipta = "bepul :)"
+#     print(f"chipta narxi:{chipta}")    
+#     sorov = input("yana davom ettirishni istaysizmi?(ha/yo'q)")
+#     if sorov != "ha":
+#         break    
+# print("dasteu tugadi!")   
+# 3-mashq
+# savol ="Kiritilgan sonning ildizini qaytaruvchi dastur.\n"
+# savol += "Musbat son kiriting "
+# savol += "(dasturni to'xtatish uchun 'exit' deb yozing): "
+
+# while True:
+#     qiymat = input(savol)
+#     if qiymat < 0:
+#         continue
+#     elif qiymat=='Exit':
+#         break
+#     else:
+#         ildiz = float(qiymat)**(0.5)
+#         print(f"{qiymat} ning ildizi {ildiz} ga teng")
+
+    
+     
+
+
+  
+
+
+        
