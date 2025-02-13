34330df5de802a5855a18d284550143f5a748775
Bunyod <b8515574@gmail.com>
2025-02-13 11:44:04 +0500

    while(new)
    changed

--- while.py
+++ while.py
@@ -55,6 +55,66 @@
 #         ildiz = float(qiymat)**(0.5)
 #         print(f"{qiymat} ning ildizi {ildiz} ga teng")
 
+#while in lists
+# 1-mashq
+# savol = "Buyurtma berishingiz mumkin:)"
+# i = 1
+# buyurtmalar = []
+# while True:
+#     sorov = input(savol+f" {i}.")
+#     buyurtmalar.append(sorov.title())
+#     sorov2 = input("yana qo'shishni hohlaysizmi?(ha/yo'q)")
+#     i+=1
+#     if sorov2 != "ha":
+#         break
+
+# print("\n Buyurtmalaringiz:")
+# for buyur in buyurtmalar:
+#     print(buyur)
+
+#2-mashq
+# print("Maxsulotlarni kiritish dasturiga xush kelibsiz! ")
+# e_bozor = {}
+# savol = "elementlar qoshish:"
+# satr = ""
+# savol2 = "narxni kiritish:"
+# narx = ""
+# i = 1
+# while True:
+#     satr  = input(savol+f" {i}.")
+#     narx = input(savol2 + f" {i}.")
+#     e_bozor[satr] = narx
+#     i+=1
+#     end = input("yana maxsulot qo'shasizmi?(ha/yo'q)")
+#     if end != "ha":
+#         break
+# print("kiritilgan maxsulotlar:")
+# for k,v in e_bozor.items():
+#     print(f"{k} maxsulotimiz : {v} narxda")
+
+#3-mashq
+buyurtmalar = ["olma","anjir","shaftoli","behi"]
+maxsulotlar = {
+    "olma":10000,
+    "shaftoli":30000,
+    "behi":50000,
+    "anor":45000
+}
+
+while buyurtmalar:
+    for buyur in buyurtmalar:
+        if buyur in maxsulotlar:
+            print(f"{buyur} narxi {maxsulotlar[buyur]}")
+            buyurtmalar.remove(buyur)
+        else:
+            print(f"Bizda {buyur} maxsuloti yo'q!")
+            buyurtmalar.remove(buyur)
+print("dastur yakunlandi.")
+
+
+
+
+
     
      
 
