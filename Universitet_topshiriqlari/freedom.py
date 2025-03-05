f0b895fd0a619c230516e7916264f3be78eb7922
bunyod <bunyodabduvaitov005@gmail.com>
2025-03-05 11:12:21 +0500

    Update freedom.py

--- Universitet_topshiriqlari/freedom.py
+++ Universitet_topshiriqlari/freedom.py
@@ -1,18 +1,18 @@
 
-# ask = int(input("Enter a number: "))
-# i =2
-#
-# while i < ask:
-#     j=1
-#     count =0
-#     while j <= i:
-#         if i % j == 0:
-#             count += 1
-#         j+=1
-#     if count ==2:
-#         print(i)
-#     i+=1
-#     count =0
+ask = int(input("Enter a number: "))
+i =2
+
+while i < ask:
+    j=1
+    count =0
+    while j <= i:
+        if i % j == 0:
+            count += 1
+        j+=1
+    if count ==2:
+        print(i)
+    i+=1
+    count =0
 
 
 
